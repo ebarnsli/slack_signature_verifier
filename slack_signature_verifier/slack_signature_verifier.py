fe2c30dcc0799089282d7aa2139b241ba2aee5c6
ebarnsli <ebarns@labelinsight.com>
2019-08-05 11:22:54 -0500

    initial

---
+++ slack_signature_verifier/slack_signature_verifier.py
@@ -0,0 +1,13 @@
+import hashlib
+import hmac
+
+
+def verify_slack_signature(slack_post_request, slack_signing_secret):
+    slack_signature = slack_post_request['headers']['X-Slack-Signature']
+    slack_request_timestamp = slack_post_request['headers']['X-Slack-Request-Timestamp']
+    request_body = slack_post_request["body"]
+
+    basestring = f"v0:{slack_request_timestamp}:{request_body}".encode('utf-8')
+    my_signature = 'v0=' + hmac.new(slack_signing_secret, basestring, hashlib.sha256).hexdigest()
+
+    return hmac.compare_digest(my_signature, slack_signature)
