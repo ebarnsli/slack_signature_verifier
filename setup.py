ec31c7aa7baf520ec1ac9c2dbd8e35234990e815
ebarnsli <ebarns@labelinsight.com>
2019-08-05 15:28:37 -0500

    readme fix

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # from distutils.core import setup
 from setuptools import setup
-with open("README.md", "r") as fh:
+with open("README", "r") as fh:
     long_description = fh.read()
 setup(
     name='slack_signature_verifier',  # How you named your package folder (MyLib)
     packages=['slack_signature_verifier'],  # Chose the same as "name"
-    version='0.4',  # Start with a small number and increase it with every change you make
+    version='0.6',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Verify your request from slack with your slack signing secret',
     long_description=long_description,
@@ -15,7 +15,7 @@ setup(
     author_email='ebarns@labelinsight.com',  # Type in your E-Mail
     url='https://github.com/ebarnsli/slack_signature_verifier',
     # Provide either the link to your github or to your website
-    download_url='https://github.com/ebarnsli/slack_signature_verifier/archive/0.0.6.tar.gz',  # I explain this later on
+    download_url='https://github.com/ebarnsli/slack_signature_verifier/archive/0.0.7.tar.gz',  # I explain this later on
     keywords=['SLACK', 'SIGNING', 'SIGNATURE'],  # Keywords that define your package best
     classifiers=[
         'Development Status :: 3 - Alpha',
