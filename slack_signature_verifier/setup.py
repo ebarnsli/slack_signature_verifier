2ee5db2fe3d5722029f3c47f856d9cf170e2d719
ebarnsli <ebarns@labelinsight.com>
2019-08-05 11:33:01 -0500

    v bump

--- slack_signature_verifier/setup.py
+++ slack_signature_verifier/setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='ebarns@labelinsight.com',  # Type in your E-Mail
     url='https://github.com/ebarnsli/slack_signature_verifier',
     # Provide either the link to your github or to your website
-    download_url='https://github.com/ebarnsli/slack_signature_verifier/archive/0.0.1.tar.gz',  # I explain this later on
+    download_url='https://github.com/ebarnsli/slack_signature_verifier/archive/0.0.2.tar.gz',  # I explain this later on
     keywords=['SLACK', 'SIGNING', 'SIGNATURE'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'hashlib',
